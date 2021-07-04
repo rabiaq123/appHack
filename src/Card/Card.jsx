649c5b4de15a4f05e356ac163f9031a05c34acdd
ZaynAl-J <zaynabbas98@gmail.com>
2021-07-03 21:02:09 -0400

    first commit

---
+++ src/Card/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Card.css';
+
+const Card = (props) => (
+    <div className="card-container">
+        <div className="card">
+            <div className="front">
+                <div className="ques">{props.ques}</div>
+            </div>
+            <div className="back">
+                <div className="ans">{props.ans}</div>
+            </div>
+        </div>
+    </div>
+)
+
+
+export default Card;
---
+++ src/Config/Firebase/db_config.js
@@ -0,0 +1,11 @@
+export const DB_CONFIG = {
+    
+    apiKey: "AIzaSyB6jKbpNSA8zQ4F8IC6wrEinNxfBKL6OHI",
+    authDomain: "studybuddy-4688b.firebaseapp.com",
+    projectId: "studybuddy-4688b",
+    storageBucket: "studybuddy-4688b.appspot.com",
+    messagingSenderId: "388107351814",
+    appId: "1:388107351814:web:519fc1a04fa3f565b61087",
+    measurementId: "G-2XT1P1SE08"
+      
+}
